b09ebc2368e02bb18851bd75cf9ae35cfac35a71
Do Quoc Huy <doh8733@gmail.com>
2022-08-25 08:43:02 +0700

    update

---
+++ app/src/main/java/com/example/jetpack_b1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jetpack_b1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        aaaa
+    }
+}
